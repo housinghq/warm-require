bf47faa228f18e02e361d7742f2552d7981863a7
Aziz Khambati <aziz.h.khambati@gmail.com>
2016-04-19 18:49:16 +0530

    Use node's module.js functions

--- index.js
+++ index.js
@@ -2,43 +2,11 @@ var watch = require('./chokidar-config');
 var loader = require('./module-loader');
 var errors = require('./errors');
 
-function dummyTranspiler(str) {
-	return str;
-}
-
-function objTranspiler(obj, str, filename) {
-	var extMatch = filename.match(/\.[^\/]*$/);
-	if (extMatch && extMatch.length) {
-		var ext = extMatch[0].slice(1);
-		if (typeof(obj[ext]) === 'function' ) {
-			return obj[ext](str);
-		}
-	}
-
-	return str;
-}
-
 function onChange(file) {
 	loader.burst(file);
 };
 
 exports.watch = function(config) {
 	var watcher = watch(config, onChange);
-
-	if (!config.transpiler) {
-		config.transpiler = dummyTranspiler;
-	} else if ( typeof(config.transpiler) === 'object' ) {
-		config.transpiler = objTranspiler.bind(null, config.transpiler);
-	}
-	if ( typeof(config.transpiler) !== 'function' ) {
-		throw errors.TRANSPILER_NON_FUNCTION;
-	}
-
-	if (!config.exts) {
-		config.exts = ['js'];
-	} else if (config.exts.indexOf('js') === -1) {
-		config.exts.push('js');
-	}
-
-	return loader.warmRequire.bind(null, config);
+	return loader.warmRequire;
 };
--- module-loader.js
+++ module-loader.js
@@ -1,4 +1,4 @@
-// NPM Packages
+var Module = module.constructor;
 var fs = require('graceful-fs');
 var path = require('path');
 
@@ -6,181 +6,72 @@ var path = require('path');
 var errors = require('./errors');
 
 // Variables
-var moduleCache = {},
-	funcCache = {},
-	depsTree = {},
-	absCacheMap = {},
-	nodePaths = [];
-
-if (process.env.NODE_PATH) {
-	nodePaths = process.env.NODE_PATH.split(';');
-}
-
-function isEmpty(obj) {
-	for (var key in obj) {
-		return false;
-	}
-	return true;
-}
+var depsTree = {},
+	loading = {},
+	absCacheMap = {};
 
 // Not Pure
-function recursiveBurst(dependent) {
-	moduleCache[dependent.absolute] = null;
-	if (dependent.cachePath) {
-		moduleCache[dependent.cachePath] = null;
-		if (depsTree[dependent.cachePath])
-			depsTree[dependent.cachePath].forEach(recursiveBurst);
-	}
-	if (!depsTree[dependent.absolute]) return;
-	return depsTree[dependent.absolute].forEach(recursiveBurst);
+function recursiveBurst(filename) {
+	delete Module._cache[require.resolve(filename)];
+	if (!depsTree[filename]) return;
+	return depsTree[filename].forEach(recursiveBurst);
 }
 
 // Not Pure
 function burst(filename) {
-	funcCache[filename] = null;
-	if (absCacheMap[filename]) {
-		funcCache[absCacheMap[filename]] = null;
-	}
-	return recursiveBurst({
-		absolute: filename,
-		cachePath: absCacheMap[filename]
-	});
-}
-
-function checkPath(file, exts) {
-	var stats;
-	try {
-		stats = fs.statSync(file);
-		if (stats.isDirectory()) {
-			return checkPath(file + '/index', exts);
-		}
-		return file;
-	} catch (e) {
-		// Do nothing. Drink Tea.
-	}
-	for (var i=0; i<exts.length; i++) {
-		try {
-			stats = fs.statSync(file + '.' + exts[i]);
-			if (!stats.isDirectory()) {
-				return file + '.' + exts[i];
-			}
-		} catch (e2) {
-			// Drink Ice Tea, because you took that long to drink warm tea.
-		}
-	}
-	return null;
-}
-
-function absolutePath(folder, file, exts) {
-	if (file.charAt(0) === '.') {
-		if (!folder) {
-			throw errors.NON_ABSOLUTE_PATH;
-		}
-		return checkPath(path.resolve(folder, file), exts);
-	}
-	if (file.charAt(0) === '/') {
-		return checkPath(file, exts);
-	}
-	for (var i=0; i<nodePaths.length; i++) {
-		var test = checkPath( path.resolve(nodePaths[i], file), exts );
-		if ( test ) {
-			return test;
-		}
-	}
-	return false;
+	delete Module._cache[require.resolve(filename)];
+	return recursiveBurst(filename);
 }
 
 // Not Pure
-function addDependent(dependent, absolute) {
+function addDependent(dependent, filename) {
 	if (!dependent) {
 		return
 	}
 
-	if (!depsTree[absolute]) {
-		depsTree[absolute] = [];
+	if (!depsTree[filename]) {
+		depsTree[filename] = [];
 	}
-	if (!depsTree[absolute].some(function(temp) {
-		return temp.absolute === dependent.absolute;
-	}) ) {
-		depsTree[absolute].push(dependent);
+	if (depsTree[filename].indexOf(dependent) === -1) {
+		depsTree[filename].push(dependent);
 	}
 }
 
-// Not Pure
-function warmRequire(config, folder, dependent, filename) {
-	if ( (arguments.length < 2) || (arguments.length > 4) )
+function warmRequire(request, parent) {
+	if ( (arguments.length < 1) || (arguments.length > 2) )
 		throw errors.INVALID_ARGUMENTS;
 
+	var filename = Module._resolveFilename(request, parent);
 	if (!filename) {
-		filename = folder;
-		folder = null;
+		throw errors.FILE_DOES_NOT_EXIST;
 	}
 
-	var cachePath;
-	if (filename.charAt(0) !== '.') {
-		cachePath = filename;
-		if (moduleCache[cachePath]) {
-			return moduleCache[cachePath];
-		}
-
+	var cachedModule = Module._cache[filename];
+	if (cachedModule) {
+		return cachedModule.exports;
 	}
 
-	var absolute = absolutePath(folder, filename, config.exts);
-	if (!absolute) {
-		if (!cachePath) {
-			throw errors.FILE_DOES_NOT_EXIST;
-		}
-		return moduleCache[cachePath] = require(filename);
+	if (loading[filename]) {
+		throw {
+			message: 'Circular dep: ' + filename
+		};
+	}
+	loading[filename] = true;
+
+	if (parent) {
+		addDependent(parent.filename, filename);
 	}
 
-	if (!cachePath) {
-		cachePath = absolute;
-	} else {
-		absCacheMap[absolute] = cachePath;
-		addDependent(dependent, cachePath);
+	var m = new Module(filename);
+	m.require = function(targetPath) {
+		return warmRequire(targetPath, m);
 	}
-	addDependent(dependent, absolute);
-	if (moduleCache[absolute]) {
-		return moduleCache[absolute];
-	}
+	m.load(filename);
+	Module._cache[filename] = m;
 
-	var func = funcCache[absolute] || funcCache[cachePath];
-	if (!func) {
-		var code = config.transpiler( fs.readFileSync(absolute, {
-			encoding: 'utf-8'
-		}), absolute);
-
-		func = funcCache[absolute] = new Function('require', 'module', 'exports', code);
-		if (cachePath) {
-			funcCache[cachePath] = funcCache[absolute];
-		}
-	}
-
-	var targetFolder = path.dirname(absolute);
-	var targetDependent = {
-		absolute: absolute
-	};
-	if (cachePath) {
-		targetDependent.cachePath = cachePath;
-	}
-	var relativeRequire = warmRequire.bind(
-		null,
-		config,
-		targetFolder,
-		targetDependent
-	);
-
-	var mod = {}, exp = {};
-	mod.exports = {};
-
-	func(relativeRequire, mod, exp);
-	moduleCache[absolute] = isEmpty(exp) ? mod.exports : exp;
-	if (cachePath) {
-		moduleCache[cachePath] = moduleCache[absolute];
-	}
-
-	return moduleCache[absolute];
-}
+	loading[filename] = false;
+	return m.exports;
+};
 
 exports.burst = burst;
 exports.warmRequire = warmRequire;
